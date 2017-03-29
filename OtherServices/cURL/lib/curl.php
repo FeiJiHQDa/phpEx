7e0066d6405934bceed603ebef7de3d34ae70888
whchao <feijihqda@126.com>
2017-03-30 00:12:03 +0800

    add curl

--- OtherServices/cURL/lib/curl.php
+++ OtherServices/cURL/lib/curl.php
@@ -1,7 +1,17 @@
 <?php
+
 /**
+ * A base curl
+ *
  * Created by PhpStorm.
  * User: HJKLI
  * Date: 2017/3/30
  * Time: 0:03
+ * @package curl
+ * @author whchao <feijihqda@126.com>
  */
+
+class curl {
+
+
+}
