c27648ecd2d158b6d893c814a1ad8d18f4b634a2
FeiJiHQDa <feijihqda@126.com>
2017-03-30 18:01:43 +0800

    add preg

--- string/preg_match_all.php
+++ string/preg_match_all.php
@@ -5,3 +5,9 @@
  * Date: 2017/3/30
  * Time: 17:59
  */
+
+preg_match_all("|<[^>]+>(.*)</[^>]+>|U",
+    "<b>example: </b><div align=left>this is a test</div>",
+    $out, PREG_PATTERN_ORDER);
+
+print_r($out);
