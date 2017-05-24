551f4e99425c89a95d29a79648f60ce479aaccc8
wuhaichao <feijihqda@126.com>
2017-05-24 15:47:05 +0800

    add system.php

---
+++ ProcessControlExtensions/programExecution/system.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: HJKLI
+ * Date: 2017/5/24
+ * Time: 15:45
+ */
+
+echo '<pre>';
+
+// 输出 shell 命令 "ls" 的返回结果
+// 并且将输出的最后一样内容返回到 $last_line。
+// 将命令的返回值保存到 $retval。
+$last_line = system('ls', $retval);
+
+// 打印更多信息
+echo '
+</pre>
+<hr />Last line of the output: ' . $last_line . '
+<hr />Return value: ' . $retval;
